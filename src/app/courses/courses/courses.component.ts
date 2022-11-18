73c61cea1a15ba6f1b128b919101d603ef52de29
carloshenriquejk <carloscardosojk@gmail.com>
2022-11-18 16:12:34 -0300

    table

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
 
+export class CoursesComponent {
+ courses: Course[] = [];
+ displayedColumns = ['name', 'category'];
 }
