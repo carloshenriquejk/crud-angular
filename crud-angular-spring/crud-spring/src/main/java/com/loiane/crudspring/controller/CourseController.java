bc0986e811fe6de4d62a0f1420f59d1161d32957
carloshenriquejk <carloscardosojk@gmail.com>
2022-11-22 15:45:27 -0300

    api

---
+++ crud-angular-spring/crud-spring/src/main/java/com/loiane/crudspring/controller/CourseController.java
@@ -0,0 +1,15 @@
+package com.loiane.crudspring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+  public List<Object> list() {
+    return null;
+  }
+
+}
